ef29a15f2129b624b9950a8b14c853069b5fc9d1
User <User@DESKTOP-JE4K2UK>
2025-03-12 22:14:37 +0100

    Add project files.

---
+++ Seminar2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Usht1
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Shkruaj nje numer per te afishuar: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write(i + " ");
+        }
+    }
+}
